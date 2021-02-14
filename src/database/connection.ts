de97f9ab8be768f194922164a963518a2216d05d
REBECANONATO <rebecanonato89@gmail.com>
2021-02-14 14:13:56 -0300

    database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,10 +4,10 @@ import path from 'path';
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        database: 'ratinges',
-        user: 'postgres',
-        password: 'postgres',
+        host: 'ec2-67-202-63-147.compute-1.amazonaws.com',
+        database: 'd4j6i5vcg8hnj5',
+        user: 'profgphculzqid',
+        password: 'a61d70b1a8270bd8882925da503f9022e44f98434658138ac5307952242baf2e',
         filename: path.resolve(__dirname, 'ratinges.pg'),
     },
 });
