1a7afb572c20987291930f0963b8fd30b5c4d9f3
REBECANONATO <rebecanonato89@gmail.com>
2021-02-14 13:39:52 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'rating-module-front.vercel.app' }));
 app.use(express.json());
 app.use(routes);
 
